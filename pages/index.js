e45af076a20c5fdbda4ffcc4b60eb7a35327e972
Julian Alvarez <julianjear10@gmail.com>
2023-01-05 21:42:32 -0500

    Connected frontend with backend

--- pages/index.js
+++ pages/index.js
@@ -18,6 +18,28 @@ import {
 
 const Home = () => {
   const [userInput, setUserInput] = useState("");
+  const [apiOutput, setApiOutput] = useState("");
+  const [isGenerating, setIsGenerating] = useState(false);
+
+  const callGenerateEndpoint = async () => {
+    setIsGenerating(true);
+
+    console.log("Calling OpenAI...");
+    const response = await fetch("/api/generate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ userInput }),
+    });
+
+    const data = await response.json();
+    const { output } = data;
+    console.log("OpenAI replied...", output.text);
+
+    setApiOutput(`${output.text}`);
+    setIsGenerating(false);
+  };
 
   return (
     <div className="root">
@@ -36,10 +58,31 @@ const Home = () => {
             h={"200px"}
             fontSize="20px"
             padding="8px"
+            placeholder="start typing here"
             value={userInput}
-            onChange={(newValue) => setUserInput(newValue)}
+            onChange={(event) => setUserInput(event.target.value)}
           />
         </div>
+
+        <div className="prompt-buttons">
+          <a className="generate-button" onClick={callGenerateEndpoint}>
+            <div className="generate">
+              <p>Generate</p>
+            </div>
+          </a>
+        </div>
+        {apiOutput && (
+          <div className="output">
+            <div className="output-header-container">
+              <div className="output-header">
+                <h3>Output</h3>
+              </div>
+            </div>
+            <div className="output-content">
+              <p>{apiOutput}</p>
+            </div>
+          </div>
+        )}
       </div>
       <div className="badge-container grow">
         <a href="https://buildspace.so/builds/ai-writer" target="_blank" rel="noreferrer">
